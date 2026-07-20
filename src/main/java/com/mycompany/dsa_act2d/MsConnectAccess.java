cf8520f8f24ef439029ad91d35db5403727f4bbc
CL2-PC <CL2-PC@CL2-PC27>
2026-07-20 15:32:25 +0800

    RPS AND CALCU

---
+++ src/main/java/com/mycompany/dsa_act2d/MsConnectAccess.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.dsa_act2d;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author CL2-PC
+ */
+public class MsConnectAccess {
+    
+    public static Connection conn() {
+        try {
+            String url = "jdbc:ucanaccess://C://Users//CL2-PC//Documents//Database1.accdb";
+            Connection conn = DriverManager.getConnection(url);
+            return conn;
+        } catch (SQLException e){
+            JOptionPane.showMessageDialog(null, e);
+        }
+        return null;
+    
+    }
+}
